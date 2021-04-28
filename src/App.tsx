0a2c33752ffbbdcc3e6c874ae3511d74c41f94eb
Priyadhars-00s <priyadh.10decoders@gmail.com>
2021-04-28 19:52:41 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './pages/Users/User';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <User />
+    </div>
+  );
+}
+
+export default App;
