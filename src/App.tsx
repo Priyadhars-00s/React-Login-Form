50744a3dfc060a4096c685fdbca28054a3ca5266
Priyadhars-00s <priyadh.10decoders@gmail.com>
2021-04-30 19:33:03 +0530

    CodeChanges

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import User from './pages/Users/User';
 import './App.css';
-
+import Test from './context'
 const App: React.FC = () => {
   return (
     <div>
       <User />
+      <Test />
     </div>
   );
 }
